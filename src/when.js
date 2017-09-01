889c1dbd7e5a9494d733423563126a214e803fbd
dihaibo <dihaibo@hujiang.com>
2017-09-01 20:28:22 +0800

    update: when

--- src/when.js
+++ src/when.js
@@ -4,12 +4,9 @@ export default (condition, effect) => {
     if (typeof condition !== 'function' || typeof effect !== 'function') {
         throw new Error('condition or effect is not function');
     }
-    const dispose = autorun(() => {
+    return autorun(() => {
         if (condition()) {
             effect();
         }
     });
-    return () => {
-        dispose();
-    };
 };
